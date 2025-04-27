e0d75dccf3731ad3116b509f5f2405f10c4d5074
Mahdi Chaabani <156582467+MahdiChaabani@users.noreply.github.com>
2025-04-27 15:16:20 +0100

    Add project files.

---
+++ Models/MenuItemModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace restaurent_hamhamma.Models
+{
+    public class MenuItemModel
+    {
+        public int ItemId { get; set; }
+        public string Name { get; set; }
+        public bool Disponible { get; set; }
+        public string Description { get; set; }
+        public float Prix { get; set; }
+        public string ImagePath { get; set; }
+
+        // Navigation properties
+        public ICollection<ClientFeedback> Feedbacks { get; set; }
+        public ICollection<Reservation> Reservations { get; set; }
+    }
+}
---
+++ Pages/LoginPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Oracle.ManagedDataAccess.Client;
+
+namespace restaurent_hamhamma.Pages
+{
+    public partial class LoginPage : Page
+    {
+        OracleConnection conn = new OracleConnection(@"user id=system;password=09535846;Data Source=localhost:1521/XE");
+
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+        private void BtnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            string username = UsernameTextBox.Text;  // Récupérer le nom d'utilisateur
+            string password = PasswordBox.Password; // Récupérer le mot de passe
+
+            try
+            {
+                conn.Open();
+
+                string query = "SELECT COUNT(*) FROM admin_tab WHERE username = :username AND pwd = :password";
+
+                OracleCommand cmd = new OracleCommand(query, conn);
+                cmd.Parameters.Add(new OracleParameter("username", username));
+                cmd.Parameters.Add(new OracleParameter("password", password));
+
+                int userCount = Convert.ToInt32(cmd.ExecuteScalar());
+
+                if (userCount > 0)
+                {
+                    
+                    NavigationService.Navigate(new AdminPage());
+                    MessageBox.Show("Login successful!");
+                }
+                else
+                {
+                    MessageBox.Show("Invalid username or password.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+    }
+}
